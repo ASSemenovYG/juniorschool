972c805c5428a8705a1dfe0f3fbe1450766a2d73
ASSemenovYG <assemenov@fil-it.ru>
2024-02-13 04:03:42 +0300

    homework1

--- src/main/java/ru/liga/dcs/lesson01/DuplicateRemover01.java
+++ src/main/java/ru/liga/dcs/lesson01/DuplicateRemover01.java
@@ -2,13 +2,28 @@ package ru.liga.dcs.lesson01;
 
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class DuplicateRemover01 {
 
     public List<String> removeDuplicates(List<String> originalList) {
-        return Collections.emptyList();
+        if (originalList != null) {
+            //при попытке сделать remove из originalList ошибка UnsupportedOperationException, т.к. в тесте создается Unmodifiable collection
+            List<String> returnList = new ArrayList<>(originalList);
+            String currEl = null;
+            List<String> copyList;
+            for (int i = 0; i < returnList.size(); i++) {
+                currEl = returnList.get(i);
+                copyList = returnList.subList(i + 1, returnList.size());
+                if (copyList.contains(currEl)) {
+                    for (int y = i + 1; y < returnList.size(); y++) {
+                        if (Objects.equals(returnList.get(y), currEl)) returnList.remove(y);
+                    }
+                }
+            }
+            return returnList;
+        } else {
+            return null;
+        }
     }
-
 }
