8f90bc1696e2a7566ea0125dc078fd8a4f79ad1b
ASSemenovYG <assemenov@fil-it.ru>
2024-02-14 20:04:24 +0300

    homework2 task1&2

--- src/main/java/ru/liga/dcs/lesson02/CharacterCounter01.java
+++ src/main/java/ru/liga/dcs/lesson02/CharacterCounter01.java
@@ -6,11 +6,17 @@ public class CharacterCounter01 {
      * Подсчитывает количество вхождений заданного символа в строку.
      *
      * @param str строка, в которой осуществляется поиск.
-     * @param ch символ, количество вхождений которого нужно подсчитать.
+     * @param ch  символ, количество вхождений которого нужно подсчитать.
      * @return количество вхождений символа в строку.
      */
     public int countCharacterOccurrences(String str, char ch) {
-        // Реализация метода
-        throw new UnsupportedOperationException("Необходимо реализовать метод");
+        String symbol = String.valueOf(ch);
+        int symbolCount = 0;
+        while (str.contains(symbol)) {
+            symbolCount++;
+            str = str.substring(str.indexOf(symbol) + 1);
+            if (str.indexOf(symbol) == str.length() - 1) break;
+        }
+        return symbolCount;
     }
 }
