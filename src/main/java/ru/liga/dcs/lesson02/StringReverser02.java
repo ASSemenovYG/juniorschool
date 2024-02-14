36b4d7e9f7e94cf3507b53745c141e21115739e8
ASSemenovYG <assemenov@fil-it.ru>
2024-02-14 20:04:24 +0300

    homework2 task1&2

--- src/main/java/ru/liga/dcs/lesson02/StringReverser02.java
+++ src/main/java/ru/liga/dcs/lesson02/StringReverser02.java
@@ -1,7 +1,14 @@
 package ru.liga.dcs.lesson02;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class StringReverser02 {
-    
+
     /**
      * Переворачивает порядок слов в предложении.
      *
@@ -9,6 +16,12 @@ public class StringReverser02 {
      * @return строка с перевернутыми словами.
      */
     public String reverseWords(String sentence) {
-        throw new UnsupportedOperationException("Необходимо разработать метод");
+        List<String> sentenceParts = Pattern.compile("\\s+|\\S+")
+                .matcher(sentence)
+                .results()
+                .map(MatchResult::group).toList();
+        List<String> reversedSentence = new ArrayList<>();
+        for (int i = sentenceParts.size() - 1; i >= 0; i--) reversedSentence.add(sentenceParts.get(i));
+        return reversedSentence.stream().map(Objects::toString).collect(Collectors.joining(""));
     }
 }
