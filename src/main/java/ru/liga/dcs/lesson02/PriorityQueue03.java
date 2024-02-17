d61bd6d6c505df5dd88caafed6176529f817a231
ASSemenovYG <assemenov@fil-it.ru>
2024-02-17 03:16:33 +0300

    homework2 task3

--- src/main/java/ru/liga/dcs/lesson02/PriorityQueue03.java
+++ src/main/java/ru/liga/dcs/lesson02/PriorityQueue03.java
@@ -1,34 +1,80 @@
 package ru.liga.dcs.lesson02;
 
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 /**
  * Цель: Написать программу на Java, реализующую очередь с приоритетами.
  * Эта структура данных должна поддерживать вставку элементов с определенным приоритетом
  * и извлечение элемента с наивысшим приоритетом.
  * Также написать хорошие джавадоки и добавить ещё тестов
  * Чем меньше число тем выше приоритет
- * @param <T>
+ *
+ * @param <T> - Object
  */
 public class PriorityQueue03<T> {
 
+    //TODO: написать джавадоки
+    /*
+    Что такое T:
+    https://docs.oracle.com/javase/tutorial/java/generics/types.html
+
+    private T t;
+
+    public void set(T t) {
+        this.t = t;
+    }
+
+    public T get() {
+        return t;
+    }*/
+
+    //элемент очереди
+    class QueueItem {
+        private final T t;
+        private final int priority;
+
+        QueueItem(T t, int priority) {
+            this.t = t;
+            this.priority = priority;
+        }
+    }
+
+    private final LinkedList<QueueItem> queue;
+
     // Конструктор
     public PriorityQueue03() {
-        // Инициализация очереди
+        this.queue = new LinkedList<>();
     }
 
     // Вставляет элемент с указанным приоритетом
     public void insert(T item, int priority) {
-        // Реализация метода
+        if (queue.isEmpty()) {
+            queue.add(new QueueItem(item, priority));
+        } else {
+            ListIterator<QueueItem> iterator = queue.listIterator();
+            int index = 0;
+            while (iterator.hasNext()) {
+                index = iterator.nextIndex();
+                if (iterator.next().priority <= priority) break;
+            }
+            //queue.add((index == queue.size() ? queue.size() : (index == 0) ? 1 : index), new QueueItem(item, priority));
+            queue.add(((index == 0) ? 1 : index), new QueueItem(item, priority));
+        }
+
     }
 
     // Извлекает и возвращает элемент с наивысшим приоритетом
     public T extractMax() {
-        // Реализация метода
-        return null; // Заглушка
+        if (queue.peekFirst() != null) {
+            return queue.pollFirst().t;
+        } else {
+            return null;
+        }
     }
 
     // Проверяет, пуста ли очередь
     public boolean isEmpty() {
-        // Реализация метода
-        return true; // Заглушка
+        return queue.isEmpty();
     }
 }
