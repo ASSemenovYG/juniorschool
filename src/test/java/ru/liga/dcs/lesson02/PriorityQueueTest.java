660023b428657c04e32ada323cefa7548172b530
ASSemenovYG <assemenov@fil-it.ru>
2024-02-19 22:43:08 +0300

    homework2
    rewrote PriorityQueue03, added JavaDocs
    added tests to PriorityQueueTest

--- src/test/java/ru/liga/dcs/lesson02/PriorityQueueTest.java
+++ src/test/java/ru/liga/dcs/lesson02/PriorityQueueTest.java
@@ -37,5 +37,66 @@ public class PriorityQueueTest {
         assertNull(queue.extractMax());
     }
 
-    // Дополнительные тесты могут быть добавлены здесь
+    @Test
+    public void testInsertAndExtractFourIterations() {
+        queue.insert(1, 1);
+        queue.insert(3, 3);
+        queue.insert(4, 4);
+        queue.insert(6, 6);
+        queue.insert(2, 2);
+
+        assertFalse(queue.isEmpty());
+        assertEquals(Integer.valueOf(1), queue.extractMax());
+        assertEquals(Integer.valueOf(2), queue.extractMax());
+        assertEquals(Integer.valueOf(3), queue.extractMax());
+
+        queue.insert(1, 1);
+
+        assertFalse(queue.isEmpty());
+        assertEquals(Integer.valueOf(1), queue.extractMax());
+        assertEquals(Integer.valueOf(4), queue.extractMax());
+        assertFalse(queue.isEmpty());
+
+        queue.insert(7, 7);
+
+        assertEquals(Integer.valueOf(6), queue.extractMax());
+        assertEquals(Integer.valueOf(7), queue.extractMax());
+
+        assertNull(queue.extractMax());
+        assertTrue(queue.isEmpty());
+
+        queue.insert(10, 1);
+        assertFalse(queue.isEmpty());
+    }
+
+    @Test
+    public void testInsertAndExtractSamePriority() {
+        queue.insert(1, 1);
+        queue.insert(13, 1);
+        queue.insert(12, 1);
+
+        assertEquals(Integer.valueOf(1), queue.extractMax());
+        assertEquals(Integer.valueOf(13), queue.extractMax());
+        assertEquals(Integer.valueOf(12), queue.extractMax());
+        assertNull(queue.extractMax());
+    }
+
+    @Test
+    public void testInsertAndExtractSameItemAndPriority() {
+        queue.insert(1, 1);
+        queue.insert(1, 1);
+        queue.insert(1, 1);
+        queue.insert(4, 1);
+        queue.insert(4, 4);
+
+        assertFalse(queue.isEmpty());
+        assertEquals(Integer.valueOf(1), queue.extractMax());
+        assertEquals(Integer.valueOf(1), queue.extractMax());
+        assertEquals(Integer.valueOf(1), queue.extractMax());
+        assertEquals(Integer.valueOf(4), queue.extractMax());
+        assertEquals(Integer.valueOf(4), queue.extractMax());
+        assertNull(queue.extractMax());
+    }
 }
+
+
