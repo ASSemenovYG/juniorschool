0127c99e991603027d6e4d1e30df2561a0d332e3
ASSemenovYG <assemenov@fil-it.ru>
2024-02-19 22:43:08 +0300

    homework2
    rewrote PriorityQueue03, added JavaDocs
    added tests to PriorityQueueTest

--- src/main/java/ru/liga/dcs/lesson02/PriorityQueue03.java
+++ src/main/java/ru/liga/dcs/lesson02/PriorityQueue03.java
@@ -1,35 +1,19 @@
 package ru.liga.dcs.lesson02;
 
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.ListIterator;
+import java.util.stream.Collectors;
 
 /**
- * Цель: Написать программу на Java, реализующую очередь с приоритетами.
- * Эта структура данных должна поддерживать вставку элементов с определенным приоритетом
- * и извлечение элемента с наивысшим приоритетом.
- * Также написать хорошие джавадоки и добавить ещё тестов
- * Чем меньше число тем выше приоритет
+ * Класс PriorityQueue03 представляет собой структуру очереди.
+ * Этот класс предоставляет методы по встравке элементов в очередь, извлечению элемента с наивысшим приортетом, проверке очереди на пустоту.
+ * Чем меньше число тем выше приоритет.
+ * При равном приоритете у добавляемого элемента и элемента в очереди, добавление происходит сразу после крайнего элемента с этим приоритетом
  *
- * @param <T> - Object
+ * @param <T> - элемент очереди
  */
 public class PriorityQueue03<T> {
-
-    //TODO: написать джавадоки
-    /*
-    Что такое T:
-    https://docs.oracle.com/javase/tutorial/java/generics/types.html
-
-    private T t;
-
-    public void set(T t) {
-        this.t = t;
-    }
-
-    public T get() {
-        return t;
-    }*/
-
-    //элемент очереди
     class QueueItem {
         private final T t;
         private final int priority;
@@ -38,42 +22,72 @@ public class PriorityQueue03<T> {
             this.t = t;
             this.priority = priority;
         }
+
+        public int getPriority() {
+            return priority;
+        }
+
+        /**
+         * @return Индекс элемента в родительском LinkedList
+         */
+        public int getIndex() {
+            return queue.indexOf(this);
+        }
     }
 
     private final LinkedList<QueueItem> queue;
 
-    // Конструктор
     public PriorityQueue03() {
         this.queue = new LinkedList<>();
     }
 
-    // Вставляет элемент с указанным приоритетом
+    /**
+     * Добавление элемента в очередь
+     * Если уже есть элементы с передаваемым приоритетом, добавление происходит сразу после крайнего элемента с этим приоритетом
+     *
+     * @param item     - элемент очереди
+     * @param priority - приоритет элемента
+     */
     public void insert(T item, int priority) {
         if (queue.isEmpty()) {
             queue.add(new QueueItem(item, priority));
         } else {
-            ListIterator<QueueItem> iterator = queue.listIterator();
-            int index = 0;
-            while (iterator.hasNext()) {
-                index = iterator.nextIndex();
-                if (iterator.next().priority <= priority) break;
+            //хешмап K<приоритет>/V<индекс в листе>
+            HashMap<Integer, Integer> priorityMap = queue
+                    .stream()
+                    //оставляем только элементы, у которых приоритет выше чем у добавляемого или равен
+                    .filter(queueItem -> queueItem.getPriority() <= priority)
+                    .collect(Collectors.toMap(
+                            QueueItem::getPriority,
+                            QueueItem::getIndex,
+                            (first, second) -> second,
+                            HashMap::new));
+
+            if (priorityMap.containsKey(priority)) {
+                queue.add(priorityMap.get(priority) + 1, new QueueItem(item, priority));
+            } else if (priority < queue.peekFirst().getPriority()) {
+                //если приоритет выше самого высокого текущего
+                queue.addFirst(new QueueItem(item, priority));
+            } else if (priority > priorityMap.values().stream().max(Comparator.naturalOrder()).get()) {
+                //если нет элементов с добавляемым приоритетом, но есть с приоритетом выше
+                queue.add(priorityMap.values().stream().max(Comparator.naturalOrder()).get() + 1, new QueueItem(item, priority));
+            } else {
+                //добавляемый приоритет ниже всех в очереди - добавляем в конец
+                queue.addLast(new QueueItem(item, priority));
             }
-            //queue.add((index == queue.size() ? queue.size() : (index == 0) ? 1 : index), new QueueItem(item, priority));
-            queue.add(((index == 0) ? 1 : index), new QueueItem(item, priority));
         }
 
     }
 
-    // Извлекает и возвращает элемент с наивысшим приоритетом
+    /**
+     * Извлекает из очереди и возвращает элемент с наивысшим приоритетом
+     *
+     * @return Элемент с наивысшим приоритетом, если очередь пуста - возвращает null
+     */
     public T extractMax() {
-        if (queue.peekFirst() != null) {
-            return queue.pollFirst().t;
-        } else {
-            return null;
-        }
+        return (queue.peekFirst() != null) ? queue.pollFirst().t : null;
     }
 
-    // Проверяет, пуста ли очередь
     public boolean isEmpty() {
         return queue.isEmpty();
     }
