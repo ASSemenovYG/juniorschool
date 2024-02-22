51b56b6b7401e3a11353d87c21e4eab94a5601b5
ASSemenovYG <assemenov@fil-it.ru>
2024-02-22 21:23:38 +0300

    leetcode LongestCommonPrefix

---
+++ src/test/java/ru/liga/dcs/leetcode/LongestCommonPrefixTest.java
@@ -0,0 +1,36 @@
+package ru.liga.dcs.leetcode;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LongestCommonPrefixTest {
+    private LongestCommonPrefix longestCommonPrefix;
+
+    @Before
+    public void setUp() throws Exception {
+        longestCommonPrefix = new LongestCommonPrefix();
+    }
+
+    @Test
+    public void longestCommonPrefixMatchFirstTwo() {
+        String[] testArray = {
+                "flower", "flow", "flight"};
+        assertThat(longestCommonPrefix.longestCommonPrefix(testArray)).isEqualTo("fl");
+    }
+
+    @Test
+    public void longestCommonPrefixNoneMatch() {
+        String[] testArray = {
+                "dog","racecar","car"};
+        assertThat(longestCommonPrefix.longestCommonPrefix(testArray)).isEqualTo("");
+    }
+
+    @Test
+    public void longestCommonPrefixSingleWord() {
+        String[] testArray = {
+                "a"};
+        assertThat(longestCommonPrefix.longestCommonPrefix(testArray)).isEqualTo("a");
+    }
+}
