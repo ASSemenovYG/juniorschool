d3d2436849a49649243c2cfac1821b1681707fce
ASSemenovYG <assemenov@fil-it.ru>
2024-02-22 15:31:11 +0300

    homework3 part2 LruCache05

--- src/main/java/ru/liga/dcs/lesson03/LruCache05.java
+++ src/main/java/ru/liga/dcs/lesson03/LruCache05.java
@@ -1,19 +1,28 @@
 package ru.liga.dcs.lesson03;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 /**
  * Класс представляет собой реализацию кэша, который удаляет наименее недавно использованные элементы,
  * когда достигнута его максимальная ёмкость.
  * The Least Recently Used (LRU)
  */
 public class LruCache05 {
+    private final Map<String, Object> cache;
+    private final int CAPACITY;
+    private final LinkedList<Object> usageHistory;
 
     /**
      * Конструктор для создания кэша с определённой ёмкостью.
      *
      * @param capacity Максимальная ёмкость кэша.
      */
-
     public LruCache05(int capacity) {
+        this.cache = new HashMap<>(capacity);
+        this.CAPACITY = capacity;
+        this.usageHistory = new LinkedList<>();
     }
 
     /**
@@ -23,7 +32,9 @@ public class LruCache05 {
      * @return Значение, соответствующее ключу, или {@code null}, если ключ не найден.
      */
     public Object get(String key) {
-        return null;
+        if (!cache.containsKey(key)) return null;
+        this.moveKeyToTop(key);
+        return cache.get(key);
     }
 
     /**
@@ -34,6 +45,31 @@ public class LruCache05 {
      * @param value Значение, которое нужно добавить.
      */
     public void put(String key, Object value) {
+        cache.put(key, value);
+        if (usageHistory.contains(key)) this.moveKeyToTop(key);
+        else this.addNewKeyAndRemoveLeastUsed(key);
+    }
 
+    /**
+     * Перемещает ключ использованного элемента наверх в usageHistory
+     *
+     * @param key Ключ для добавляемого/извлекаемого значения.
+     */
+    private void moveKeyToTop(String key) {
+        usageHistory.remove(key);
+        usageHistory.addFirst(key);
+    }
+
+    /**
+     * Добавляет новый ключ в начало usageHistory и удаляет из кеша самый старый использованный элемент при переполнении ёмкости
+     *
+     * @param key Ключ для добавляемого значения.
+     */
+    private void addNewKeyAndRemoveLeastUsed(String key) {
+        if (CAPACITY - usageHistory.size() > 0) usageHistory.addFirst(key);
+        else {
+            cache.remove(usageHistory.removeLast());
+            usageHistory.addFirst(key);
+        }
     }
 }
