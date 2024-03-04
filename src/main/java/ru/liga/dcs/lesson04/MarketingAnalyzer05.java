e15028939f26eb1bb6718cfd792c0a2e62b31995
ASSemenovYG <assemenov@fil-it.ru>
2024-03-04 20:32:19 +0300

    lesson 04 MarketingAnalyzer05 done && CurrencyConverter03 fix

--- src/main/java/ru/liga/dcs/lesson04/MarketingAnalyzer05.java
+++ src/main/java/ru/liga/dcs/lesson04/MarketingAnalyzer05.java
@@ -4,13 +4,16 @@ import ru.liga.dcs.lesson04.domain.Category;
 import ru.liga.dcs.lesson04.domain.MarketingImportance;
 import ru.liga.dcs.lesson04.domain.Product;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Класс для анализа маркетинговых данных о продуктах в магазине.
  */
 public class MarketingAnalyzer05 {
 
+    private final List<Product> products;
 
     /**
      * Конструктор, создающий анализатор маркетинга на основе списка продуктов.
@@ -18,6 +21,7 @@ public class MarketingAnalyzer05 {
      * @param products список продуктов
      */
     public MarketingAnalyzer05(List<Product> products) {
+        this.products = products;
     }
 
     /**
@@ -27,7 +31,12 @@ public class MarketingAnalyzer05 {
      * @return средняя цена продуктов в указанной категории
      */
     public double calculateAveragePriceByCategory(Category category) {
-        return 0.0;
+        int count = countProductsByCategory(category);
+
+        if (count == 0)
+            return 0.0;
+
+        return calculateProductSumByCategory(category) / count;
     }
 
     /**
@@ -37,7 +46,10 @@ public class MarketingAnalyzer05 {
      * @return самый дорогой продукт в указанной категории
      */
     public Product findMostExpensiveProductByCategory(Category category) {
-        return null;
+        return products.stream()
+                .filter(product -> product.getCategory() == category)
+                .max(Comparator.comparing(Product::getPrice))
+                .orElse(null);
     }
 
     /**
@@ -47,7 +59,10 @@ public class MarketingAnalyzer05 {
      * @return самый дешевый продукт в указанной категории
      */
     public Product findCheapestProductByCategory(Category category) {
-        return null;
+        return products.stream()
+                .filter(product -> product.getCategory() == category)
+                .min(Comparator.comparing(Product::getPrice))
+                .orElse(null);
     }
 
     /**
@@ -56,7 +71,41 @@ public class MarketingAnalyzer05 {
      * @return объект MarketingImportance, содержащий карту важности категорий
      */
     public MarketingImportance calculateCategoryImportance() {
-        return null;
+        HashMap<Category, Double> importanceMap = new HashMap<>();
+        for (Product product : products) {
+            if (!importanceMap.containsKey(product.getCategory())) {
+                importanceMap.put(
+                        product.getCategory(),
+                        countProductsByCategory(product.getCategory()) * calculateAveragePriceByCategory(product.getCategory())
+                );
+            }
+        }
+        return new MarketingImportance(importanceMap);
+    }
+
+    /**
+     * Вычисляет количество продуктов в указанной категории
+     *
+     * @param category категория продуктов
+     * @return количество продуктов в категории
+     */
+    private int countProductsByCategory(Category category) {
+        return (int) products.stream()
+                .filter(product -> product.getCategory() == category)
+                .count();
+    }
+
+    /**
+     * Вычисляет сумму цен продуктов в указанной категории
+     *
+     * @param category категория продуктов
+     * @return сумма цен продуктов в категории
+     */
+    private double calculateProductSumByCategory(Category category) {
+        return products.stream()
+                .filter(product -> product.getCategory() == category)
+                .mapToDouble(Product::getPrice)
+                .sum();
     }
 
 }
