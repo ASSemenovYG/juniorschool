4635cd4967941fe1773a72d969eb47eea66f4bc3
ASSemenovYG <assemenov@fil-it.ru>
2024-03-11 23:39:58 +0300

    lesson 05 PasswordGenerator05 && TreeDepth06

--- src/main/java/ru/liga/dcs/lesson05/TreeDepth06.java
+++ src/main/java/ru/liga/dcs/lesson05/TreeDepth06.java
@@ -15,6 +15,14 @@ public class TreeDepth06 {
         public TreeNode(int val) {
             this.val = val;
         }
+
+        private boolean peekLeft() {
+            return this.left != null;
+        }
+
+        private boolean peekRight() {
+            return this.right != null;
+        }
     }
 
     /**
@@ -24,6 +32,23 @@ public class TreeDepth06 {
      * @return глубина дерева
      */
     public int maxDepth(TreeNode root) {
-            return 0;
+        int result = 0;
+        if (root != null) {
+            result++;
+            result = getNextNodeDepth(root, result);
+        }
+        return result;
+    }
+
+    private int getNextNodeDepth(TreeNode node, int depth) {
+        int leftDepth = depth;
+        int rightDepth = depth;
+        if (node.peekLeft()) {
+            leftDepth = getNextNodeDepth(node.left, depth + 1);
+        }
+        if (node.peekRight()) {
+            rightDepth = getNextNodeDepth(node.right, depth + 1);
+        }
+        return Math.max(leftDepth, rightDepth);
     }
 }
