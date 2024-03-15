f9867d6a420ffaab815cb55ac7cb6f819bf78b5b
ASSemenovYG <assemenov@fil-it.ru>
2024-03-15 17:35:10 +0300

    lesson 06 PatientParser05 && BmiCalculator05

--- src/main/java/ru/liga/dcs/lesson06/BmiCalculator05.java
+++ src/main/java/ru/liga/dcs/lesson06/BmiCalculator05.java
@@ -1,13 +1,18 @@
 package ru.liga.dcs.lesson06;
 
 public class BmiCalculator05 {
+    private static final int BMI_HEIGHT_POW_VALUE = 2;
+
     /**
      * Метод для расчета индекса массы тела (ИМТ) по данным пациента.
+     * ИМТ = m/h^2
+     * m - масса тела в кг
+     * h - высота в метрах
      *
      * @param patient объект пациента
      * @return значение ИМТ
      */
     public double calculateBMI(Patient patient) {
-        return 0.0;
+        return patient.getWeight() / Math.pow(patient.getHeight(), BMI_HEIGHT_POW_VALUE);
     }
 }
--- src/main/java/ru/liga/dcs/lesson06/PatientParser05.java
+++ src/main/java/ru/liga/dcs/lesson06/PatientParser05.java
@@ -1,12 +1,33 @@
 package ru.liga.dcs.lesson06;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PatientParser05 {
+    private static final String DATA_STRING_NAME_PARAMETER = "Имя";
+    private static final String DATA_STRING_AGE_PARAMETER = "Возраст";
+    private static final String DATA_STRING_WEIGHT_PARAMETER = "Вес";
+    private static final String DATA_STRING_HEIGHT_PARAMETER = "Рост";
+    private static final String DATA_STRING_PARAMETERS_DELIMITER = ";";
+    private static final String DATA_STRING_PARAMETER_NAME_VALUE_DELIMITER = ":";
+
     /**
      * Метод для парсинга строки и создания объекта пациента.
+     *
      * @param data строка с данными о пациенте в формате "Имя:Иван;Возраст:35;Вес:80;Рост:1.75"
      * @return объект пациента
      */
     public Patient parsePatientData(String data) {
-        return null;
+        Map<String, String> patientParametersMap = new HashMap<>();
+        for (String patientParameter : data.split(DATA_STRING_PARAMETERS_DELIMITER)) {
+            String[] parameter = patientParameter.split(DATA_STRING_PARAMETER_NAME_VALUE_DELIMITER);
+            patientParametersMap.put(parameter[0], parameter[1]);
+        }
+        return new Patient(
+                patientParametersMap.get(DATA_STRING_NAME_PARAMETER),
+                Integer.parseInt(patientParametersMap.get(DATA_STRING_AGE_PARAMETER)),
+                Double.parseDouble(patientParametersMap.get(DATA_STRING_WEIGHT_PARAMETER)),
+                Double.parseDouble(patientParametersMap.get(DATA_STRING_HEIGHT_PARAMETER))
+        );
     }
 }
