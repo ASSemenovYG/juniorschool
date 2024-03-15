bd712c00e0b9db846ef986772960733873cb6bca
ASSemenovYG <assemenov@fil-it.ru>
2024-03-15 16:33:56 +0300

    lesson 06 TransactionAnalyzer03

--- src/main/java/ru/liga/dcs/lesson06/TransactionAnalyzer03.java
+++ src/main/java/ru/liga/dcs/lesson06/TransactionAnalyzer03.java
@@ -3,6 +3,9 @@ package ru.liga.dcs.lesson06;
 import java.util.*;
 
 public class TransactionAnalyzer03 {
+    private static final int NIGHT_TIME_START_HOUR = 22;
+    private static final int NIGHT_TIME_END_HOUR = 6;
+
     /**
      * Определяет подозрительные транзакции из списка на основе заданных критериев.
      * <p>
@@ -19,7 +22,19 @@ public class TransactionAnalyzer03 {
      * @throws NullPointerException если переданный список транзакций равен null.
      */
     public static List<Transaction> detectSuspiciousTransactions(List<Transaction> transactions, double thresholdAmount) {
-        return Collections.emptyList();
+        List<Transaction> result = new ArrayList<>();
+        for (Transaction t : transactions) {
+            if (t.getAmount() > thresholdAmount || !Objects.equals(t.getClientCountry(), t.getCountry()) || isNightTime(t.getTimestamp())) {
+                result.add(t);
+            }
+        }
+        return result;
+    }
+
+    private static boolean isNightTime(Date timestamp) {
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(timestamp);
+        return calendar.get(Calendar.HOUR_OF_DAY) >= NIGHT_TIME_START_HOUR || calendar.get(Calendar.HOUR_OF_DAY) <= NIGHT_TIME_END_HOUR;
     }
 
 }
