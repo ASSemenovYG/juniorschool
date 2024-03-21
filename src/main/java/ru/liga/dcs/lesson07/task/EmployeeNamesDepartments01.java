1122125e3cf4a6a4ec1d37b3502093f1c89fc0b6
ASSemenovYG <assemenov@fil-it.ru>
2024-03-21 15:53:40 +0300

    lesson 07 EmployeeNamesDepartments01

--- src/main/java/ru/liga/dcs/lesson07/task/EmployeeNamesDepartments01.java
+++ src/main/java/ru/liga/dcs/lesson07/task/EmployeeNamesDepartments01.java
@@ -1,7 +1,8 @@
 package ru.liga.dcs.lesson07.task;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * Утилитный класс для работы с сотрудниками.
@@ -12,11 +13,14 @@ public class EmployeeNamesDepartments01 {
     /**
      * Фильтрует список сотрудников по департаменту и возвращает список их имен.
      *
-     * @param employees список сотрудников
+     * @param employees  список сотрудников
      * @param department название департамента
      * @return список имен сотрудников из данного департамента
      */
     public static List<String> getEmployeeNamesByDepartment(List<Employee> employees, String department) {
-        return Collections.emptyList();
+        return employees.stream()
+                .filter(employee -> Objects.equals(employee.getDepartment(), department))
+                .map(Employee::getName)
+                .collect(Collectors.toList());
     }
 }
